dc3607111c8d51d4b2b0e5964bf4214122f0d5f7
Alexander <abernal20@unm.edu>
2026-01-02 21:15:04 -0700

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,52 @@
+import pygame 
+import sys 
+
+from const import * 
+from game import Game
+
+class Main: 
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT)) 
+        pygame.display.set_caption('Chess') 
+        self.game = Game()
+
+    def mainloop(self): 
+
+        screen = self.screen 
+        game = self.game
+        dragger = self.game.dragger
+
+        while True: 
+            game.show_bg(screen)
+            game.show_pieces(screen)
+
+            for event in pygame.event.get(): 
+
+                # click 
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    dragger.update_mouse(event.pos) 
+                    print(event.pos)
+
+                # mouse motion
+                elif event.type == pygame.MOUSEBUTTONUP: 
+                    pass 
+
+                # click release 
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    pass
+
+                # quit application 
+                if event.type == pygame.QUIT: 
+                    pygame.quit() 
+                    sys.exit()
+
+
+
+
+            pygame.display.update() 
+
+
+main = Main() 
+main.mainloop() 
